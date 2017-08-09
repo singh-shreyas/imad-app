21c8620e64712c31bda4779d2b2d0c5194bf5524
singh-shreyas <shreyassingh.98@gmail.com>
2017-08-09 22:41:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,9 +108,13 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+var names = [];
 app.get('/submit-name/:name',function(req,res){
    //Get the name from the request.
-   var name = req.params.
+   var name = req.params.name;
+   names.push(name);
+   //JSON
+   res.send(JSON.stringify(names));
 });
 
 
