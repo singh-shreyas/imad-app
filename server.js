faad888dafd7f5628988a5897969af1eea42f14e
singh-shreyas <shreyassingh.98@gmail.com>
2017-08-14 19:51:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -131,8 +131,8 @@ app.get('/articles/:articleName', function (req, res) {
            if(result.rows.length===0){
                res.status(404).send('Article not found');
            }else{
-               var articleData = result.rows[0];
-               res.send(createTemplate(articleData));
+               var articleData1 = result.rows[0];
+               res.send(createTemplate(articleData1));
            }
        }
     });
